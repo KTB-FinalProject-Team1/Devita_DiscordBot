dd66de2b33cd2374df964366b3a46cb516636a7a
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2024-11-01 17:52:54 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -2,3 +2,9 @@ import discord
 import requests
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+JENKINS_URL = os.getenv('JENKINS_URL')
+
+intents = discord.Intents.default()
