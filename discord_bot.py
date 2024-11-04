32dc8bb8cee31d495a6aca838d7cf92358b37e9d
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2024-11-04 15:31:26 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-JENKINS_URL = os.getenv('JENKINS_URL')
+JENKINS_CDURL = os.getenv('JENKINS_CDURL')
+JENKINS_OVERURL = os.getenv('JENKINS_OVERURL')
 JENKINS_USER = os.getenv('JENKINS_USER')
 JENKINS_TOKEN = os.getenv('JENKINS_TOKEN')
 
@@ -21,13 +22,25 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('!test'):
+    if message.channel.id != 1302880514608468070:
+        return
+    send_channel = client.get_channel(1302880514608468070)
+    if message.contest.startswith('!over'):
         response = requests.post(
-            JENKINS_URL,
+            JENKINS_OVERURL,
             auth=(JENKINS_USER,JENKINS_TOKEN)
         )
         if response.status_code == 201:
-            await message.channel.send("Test 파이프라인 배포 성공")
+            await send_channel.send('테스트 서버들 중지 완료')
         else:
-            await message.channel.send(f"{response.status_code}에러가 발생하였습니다")
+            await send_channel.send(f"{response.status_code}애러가 발생했습니다")
+    if message.content.startswith('!test'):
+        response = requests.post(
+            JENKINS_CDURL,
+            auth=(JENKINS_USER,JENKINS_TOKEN)
+        )
+        if response.status_code == 201:
+            await send_channel.send("Test 파이프라인 배포 성공")
+        else:
+            await send_channel.send(f"{response.status_code}에러가 발생하였습니다")
 client.run(DISCORD_TOKEN)
