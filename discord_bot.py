180b3a0687139290b5285628f96ba0bb34ab0aa4
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2024-12-09 14:55:15 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -77,7 +77,7 @@ def send_message_to_discord(message):
 def get_current_build_number(jobName):
     for i in range(10):  # 최대 10번 재시도
         url = f"http://3.34.246.115:8080/job/{jobName}/lastBuild/api/json"
-        response = requests.get(url, auth=(JENKINS_USER, JENKINS_TOKEN))
+        response = request.get(url, auth=(JENKINS_USER, JENKINS_TOKEN))
         if response.status_code == 200:
             data = response.json()
             print(f"DEBUG: 파이프라인 내용 - {data}")
